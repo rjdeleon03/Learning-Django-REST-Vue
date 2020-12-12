7a021c6563fd53560a0b7ae3d337e4ab21e1d10d
Ricardo De Leon <rjdeleon@up.edu.ph>
2020-12-13 05:19:59 +0800

    Remove secret

--- 03-DRF-LEVEL-ONE/newsapi/newsapi/settings.py
+++ 03-DRF-LEVEL-ONE/newsapi/newsapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5i#+cg(x&0eet(15d&h@qq+4o0xissc&a#(s72r!c_(wew&ost'
+SECRET_KEY = '12345'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
