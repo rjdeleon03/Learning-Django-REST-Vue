2d3eda2e0fab3a0eb9d6e1ba277eb29e2b7724a0
Ricardo De Leon <rjdeleon@up.edu.ph>
2020-12-13 04:26:22 +0800

    Support basic APIs

--- 02-WEB-APIs/First_API_Django/onlinestore/products/views.py
+++ 02-WEB-APIs/First_API_Django/onlinestore/products/views.py
@@ -1,15 +1,47 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
-
-# Create your views here.
+from django.http import JsonResponse
 from .models import Product, Manufacturer
 
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
+def product_list(request):
+    products = Product.objects.all()  # [:30]
+    data = {"products": list(products.values())}
+    response = JsonResponse(data)
+    return response
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {"product": {
+            "name": product.name,
+            "manufacturer": product.manufacturer.name,
+            "description": product.description,
+            "photo": product.photo.url,
+            "price": product.price,
+            "shipping_cost": product.shipping_cost,
+            "quantity": product.quantity,
+        }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+
+                "code": 404,
+                "message": "Product not found!"
+            }
+        }, status=404)
+
+    return response
+
+# Create your views here.
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
+
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
+
+
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "products/product_list.html"
