e13e4bfaa4c20141f426cbece49d3aded3f10f61
Ricardo De Leon <rjdeleon@up.edu.ph>
2020-12-13 03:59:23 +0800

    Add link to individual product page

--- 02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
+++ 02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProductDetailView, ProductListView
 
+app_name = "products"
 urlpatterns = [
     path("", ProductListView.as_view(), name="product-list"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail")
